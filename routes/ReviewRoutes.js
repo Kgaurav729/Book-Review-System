33b466f1c7f7d2c9d53670e3243d036d05697a1d
Gaurav <gauravkulmethe007@gmail.com>
2025-05-22 00:58:31 +0530

    small fix in ReviewRoutes

--- routes/ReviewRoutes.js
+++ routes/ReviewRoutes.js
@@ -8,8 +8,8 @@ const {
   deleteReview,
 } = require('../controllers/ReviewController');
 
-router.post('/books/:id/reviews', protect, addReview);     
-router.put('/reviews/:id', protect, updateReview);         
+router.post('/books/:id/reviews', protect,validateReview, addReview);     
+router.put('/reviews/:id', protect,validateReview, updateReview);         
 router.delete('/reviews/:id', protect, deleteReview);      
 
 module.exports = router;
